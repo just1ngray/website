845933510041eda382f59aa4962c09fd104b4f3e
Justin Gray <just1ngray@outlook.com>
2026-03-23 00:58:07 -0300

    add location context to the main window

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,20 @@
-import { ParentProps } from "solid-js";
+import { For, ParentProps } from "solid-js";
 import { GitHubIcon } from "../icons/GitHub";
 import { HomeIcon } from "../icons/Home";
 import { LinkedInIcon } from "../icons/LinkedIn";
 import { Nav } from "./Nav";
+import { useLocation } from "@solidjs/router";
+import { ChevronIcon } from "../icons/Chevron";
 
 
 export function Layout(props: ParentProps) {
+  const location = useLocation();
+
   return (
     <div class="bg-stone-800 min-h-dvh text-stone-200">
       <div class="flex flex-row">
+
+        {/* external links and a dummy 'home' link  */}
         <div class="text-stone-400 p-1 flex flex-col gap-2">
           <div class="cursor-pointer text-stone-100">
             <HomeIcon px={48} />
@@ -24,10 +30,29 @@ export function Layout(props: ParentProps) {
             </a>
           </div>
         </div>
+
+        {/* "file" navigation selection */}
         <Nav />
-        <div>
-          {props.children}
+
+        {/* main window */}
+        <div class="grow">
+          <div class="p-2 flex flex-row items-center text-stone-400">
+            <span>{window.location.origin}</span>
+            <For each={location.pathname.split("/").filter(part => part.length > 0)}>
+              {part => <>
+                <ChevronIcon />
+                <span>{part}</span>
+              </>}
+            </For>
+          </div>
+
+          <hr class="text-stone-400" />
+
+          <main class="p-2">
+            {props.children}
+          </main>
         </div>
+
       </div>
     </div>
   )
